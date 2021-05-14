cf3a10b05c0bce91a3b3b007758e320b322f3f37
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-14 11:06:47 +0700

    add email and password

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -2,21 +2,25 @@ class UserModel {
   final int id;
   final String name;
   final String username;
+  final String email;
+  final String? password;
 
   UserModel({
     required this.id,
     required this.name,
+    required this.email,
+    this.password,
     required this.username,
   });
 
   static UserModel one() {
-    return UserModel(id: 1, name: 'Admin', username: 'admin');
+    return UserModel(id: 1, name: 'Admin', username: 'admin', email: 'admin@example.com');
   }
 
   static List<UserModel> all() {
     return [
-      UserModel(id: 1, name: 'Admin', username: 'admin'),
-      UserModel(id: 2, name: 'User', username: 'user')
+      UserModel(id: 1, name: 'Admin', username: 'admin', email: 'admin@example.com'),
+      UserModel(id: 2, name: 'User', username: 'user', email: 'user@example.com')
     ];
   }
 }
