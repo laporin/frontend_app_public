4eea5632a4303074d0d2a4273af8ab9967c77b4f
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-14 16:23:00 +0700

    form

--- lib/screen/report/new_report_screen.dart
+++ lib/screen/report/new_report_screen.dart
@@ -6,6 +6,8 @@ class NewReportScreen extends StatefulWidget {
 }
 
 class _NewReportScreenState extends State<NewReportScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +19,77 @@ class _NewReportScreenState extends State<NewReportScreen> {
         centerTitle: true,
       ),
       body: Container(
-        child: Text('New Report'),
+        padding: EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Deskripsi Laporan',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.grey.shade700,
+                ),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                maxLines: null,
+                expands: true,
+                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+                  hintText: 'Tulis deskripsi laporan',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Mohon tulis deskripsi laporan';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+              Text(
+                'Kategori',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.grey.shade700,
+                ),
+              ),
+              SizedBox(height: 15),
+              Text(
+                'Lokasi',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.grey.shade700,
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Processing Data'),
+                      ),
+                    );
+                  }
+                },
+                child: Text(
+                  'Kirim Laporan',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
