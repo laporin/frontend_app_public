2dde4688dbb5c45ed7e4433042f46c467b6879d2
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-20 19:50:58 +0700

    add detail report

---
+++ lib/screen/report/detail_report_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DetailReportScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Detail'),
+        ],
+      ),
+    );
+  }
+}
