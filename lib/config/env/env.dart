b90822a71df1937bee52cbfc7886f82a1cd0a972
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-06-01 18:32:37 +0700

    add be ai url

--- lib/config/env/env.dart
+++ lib/config/env/env.dart
@@ -8,5 +8,6 @@ abstract class Env {
   static const String key = _Env.key;
   static const String environment = _Env.environment;
   static const String backendUrl = _Env.backendUrl;
+  static const String backendAiUrl = _Env.backendAiUrl;
   static const bool isStudent = _Env.isStudent;
 }
