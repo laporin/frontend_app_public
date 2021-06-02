50dc0c71015c91032eb8c53f50b23de3028bb35e
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-06-02 20:17:31 +0700

    remove abstract

--- lib/features/user/presentation/bloc/user_event.dart
+++ lib/features/user/presentation/bloc/user_event.dart
@@ -1,6 +1,6 @@
 part of 'user_bloc.dart';
 
 @freezed
-abstract class UserEvent with _$UserEvent{
+class UserEvent with _$UserEvent{
   const factory UserEvent.getAuthenticatedUserEvent() = GetAuthenticatedUserEvent;
 }
--- lib/features/user/presentation/bloc/user_state.dart
+++ lib/features/user/presentation/bloc/user_state.dart
@@ -1,7 +1,7 @@
 part of 'user_bloc.dart';
 
 @freezed
-abstract class UserState with _$UserState {
+class UserState with _$UserState {
   const factory UserState.initial() = _InitialUserState;
   const factory UserState.loading() = _LoadingUserState;
   const factory UserState.error(String message) = _ErrorUserState;
